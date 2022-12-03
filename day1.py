559c1be8bfb2495fe50b6b41cfff39d8fbdc537c
Dmitry Fomin <fomin.dm@gmail.com>
2022-12-03 11:45:47 +0100

    Day 1 part 2

--- day1.py
+++ day1.py
@@ -1,4 +1,7 @@
-def main():
+import heapq
+
+
+def top1() -> int:
     current_max = 0
     current_calories = 0
     with open("inputs/day1.txt") as f:
@@ -10,8 +13,27 @@ def main():
                 current_calories = 0
             else:
                 current_calories += int(line)
-    print(max(current_max, current_calories))
+    return max(current_max, current_calories)
+
+
+def top_n(n: int) -> int:
+    heap = []
+    current_calories = 0
+    with open("inputs/day1.txt") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) == 0:
+                if len(heap) < n:
+                    heap.append(current_calories)
+                    if len(heap) == n:
+                        heapq.heapify(heap)
+                elif heap[0] < current_calories:
+                    heapq.heapreplace(heap, current_calories)
+                current_calories = 0
+            else:
+                current_calories += int(line)
+    return sum(heap)
 
 
 if __name__ == "__main__":
-    main()
+    print(top_n(3))
